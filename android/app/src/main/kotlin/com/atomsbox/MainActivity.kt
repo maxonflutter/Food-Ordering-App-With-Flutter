b39691fdd794af4858a93558640071d4df98ed2b
maxonflutter <massimo@maxonflutter.com>
2024-03-11 09:41:39 -0700

    Removed the multiple flavors

--- android/app/src/main/kotlin/com/atomsbox/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.atomsbox.hungry
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/food_ordering_app_with_flutter_and_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.food_ordering_app_with_flutter_and_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
